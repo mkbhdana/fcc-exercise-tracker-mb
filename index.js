98c053ea654b672f41a7c1eac2a6e5b512c2d5df
mkbhdana <mkbhdana@gmail.com>
2022-07-30 14:06:25 +0530

    adding script for heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
+const port = process.env.PORT || 3000
 
 
 
@@ -15,31 +16,31 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const ExerciseSchema = new mongoose.Schema({
-    description: {
-        type: String,
-        required: true,
-        maxlength: [25, 'Description too long, not greater than 25']
-    },
-    duration: {
-        type: Number,
-        required: true,
-        min: [1, 'Duration too short, at least 1 minute']
-    },
-    date: {
-        type: Date, 
-        default: Date.now
-    },
-   userId: {
-        type: String,
-        required: true
-    }
+  description: {
+    type: String,
+    required: true,
+    maxlength: [25, 'Description too long, not greater than 25']
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: [1, 'Duration too short, at least 1 minute']
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  userId: {
+    type: String,
+    required: true
+  }
 })
 const UserSchema = new mongoose.Schema({
   username: {
-        type: String,
-        unique: true,
-        required: true
-    },
+    type: String,
+    unique: true,
+    required: true
+  },
 });
 const User = mongoose.model("User", UserSchema);
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
@@ -59,47 +60,47 @@ app.route('/api/users').get((req, res) => {
     res.json(data);
   });
 }).post((req, res) => {
-  User.findOne({"username": req.body.username}, (err, userData) =>{
-    if(err || userData) {
+  User.findOne({ "username": req.body.username }, (err, userData) => {
+    if (err || userData) {
       res.send("username already taken");
     } else {
       const newUser = new User({
-    username: req.body.username
-  })
-  newUser.save((err, data) => {
-     const reducedData = {
-          "username": data.username, 
+        username: req.body.username
+      })
+      newUser.save((err, data) => {
+        const reducedData = {
+          "username": data.username,
           "_id": data._id
         };
-    if(err || !data){
-      res.send("There was an error saving the user")
-    }else{
-      res.json(reducedData)
+        if (err || !data) {
+          res.send("There was an error saving the user")
+        } else {
+          res.json(reducedData)
+        }
+      })
     }
   })
-    }
-  })
-  
+
 })
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   const id = req.body[":_id"] || req.params._id
-  const {description, duration, date} = req.body
+  const { description, duration, date } = req.body
   User.findById(id, (err, userData) => {
-    if(err || !userData) {
+    if (err || !userData) {
       res.send("Could not find user");
-    }else{
+    } else {
       const newExercise = new Exercise({
-        userId: id, 
+        userId: id,
         description,
         duration,
         date: date || Date.now()
       })
       newExercise.save((err, data) => {
-        if(err || !data) {
+        if (err || !data) {
           res.send("There was an error saving this exercise")
-        }else {
-          const { description, duration, date, _id} = data;
+        } else {
+          const { description, duration, date, _id } = data;
           res.json({
             username: userData.username,
             description,
@@ -117,39 +118,39 @@ app.get("/api/users/:_id/logs", (req, res) => {
   const { from, to, limit } = req.query;
   const id = req.body[":_id"] || req.params._id
   User.findById(id, (err, userData) => {
-    if(err || !userData) {
+    if (err || !userData) {
       res.send("Could not find user");
-    }else{
+    } else {
       let dateObj = {}
-      if(from){
+      if (from) {
         dateObj[`$gte`] = new Date(from)
       }
-      if(to){
-        dateObj[`$lte`] = new Date(to) 
+      if (to) {
+        dateObj[`$lte`] = new Date(to)
       }
       let filter = {
-       userId: id
+        userId: id
       }
-      if(from || to ){
+      if (from || to) {
         filter.date = dateObj
       }
       let nonNullLimit = limit ?? 500
       Exercise.find(filter).limit(+nonNullLimit).exec((err, data) => {
-        if(err || !data){
+        if (err || !data) {
           res.json([])
-        }else{
+        } else {
           const count = data.length
           const rawLog = data
-          const {username, _id} = userData;
-          const log= rawLog.map((l) => ({
+          const { username, _id } = userData;
+          const log = rawLog.map((l) => ({
             description: l.description,
             duration: l.duration,
             date: l.date.toDateString()
           }))
-          res.json({username, count,from, to,  _id, log})
+          res.json({ username, count, from, to, _id, log })
         }
       })
-    } 
+    }
   })
 })
 
